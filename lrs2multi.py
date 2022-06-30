75c72ca42daf3acce873fc67cc4088c69347b6f9
grzeimann <grzeimann@gmail.com>
2022-06-30 13:31:14 -0500

    update

--- lrs2multi.py
+++ lrs2multi.py
@@ -61,8 +61,11 @@ class LRS2Multi:
         datae = f[3].data
         datae[f[3].data==0.] = np.nan
         uvmask = np.abs(wave-3736.0) < 1.6
-        data[:, uvmask] = np.nan
-        datae[:, uvmask] = np.nan
+        ind1 = np.searchsorted(wave, 3740, side='right')
+        ind2 = np.searchsorted(wave, 3734, side='left')
+        if uvmask.sum() > 0:
+            data[:, uvmask] = data[:, ind1] / 2. + data[ind2] / 2.
+            datae[:, uvmask] = datae[:, ind1] / 2. + datae[ind2] / 2.
         for i in np.arange(data.shape[0]):
             sel = np.isnan(data[i])
             for i in np.arange(1, 3):
