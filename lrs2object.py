e8764759e14c9ae186e296571022413c2df6a8c5
grzeimann <grzeimann@gmail.com>
2022-07-21 16:02:39 -0500

    debugging

--- lrs2object.py
+++ lrs2object.py
@@ -675,11 +675,11 @@ class LRS2Object:
                 _w = (l2 - wave) / (l2 - l1)
                 w.append(_w)
         sSp = np.nanmean(y, axis=0)
-        sSp[wsel] = (y[0][:, :, wsel] * w[0][:, :, wsel] +
-                     y[1][:, :, wsel] * w[1][:, :, wsel])
+        sSp[wsel] = (y[0][:, :, wsel] * w[0][wsel] +
+                     y[1][:, :, wsel] * w[1][wsel])
         esSp = np.nanmean(z, axis=0)
-        esSp[wsel] = (z[0][:, :, wsel] * (w[0][:, :, wsel]) +
-                      z[1][:, :, wsel] * (w[1][:, :, wsel]))
+        esSp[wsel] = (z[0][:, :, wsel] * (w[0][wsel]) +
+                      z[1][:, :, wsel] * (w[1][wsel]))
         return sSp, esSp
     
     def combine_spectra(self):
