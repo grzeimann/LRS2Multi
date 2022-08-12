687d06bd978f030151263fa76fd986b5480d0e3a
grzeimann <grzeimann@gmail.com>
2022-08-12 13:49:40 -0500

    update

--- fiber_utils.py
+++ fiber_utils.py
@@ -994,7 +994,7 @@ def get_fiber_to_fiber(data, error, wave, sigma=5., width=2):
         ftf[i, :20] = y[:20]
         ftf[i, -20:] = y[-20:]
         mask[i] = np.abs(data[i] - avg * ftf[i]) > (sigma * error[i])
-        for j in np.arange(1, int(width)):
+        for j in np.arange(1, int(width+1)):
             mask[i, j:] += mask[i, :-j]
             mask[i, :-j] += mask[i, j:]
     return ftf, mask
